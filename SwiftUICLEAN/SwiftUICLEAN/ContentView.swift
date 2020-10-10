cd90d0696436a14d1cf966a5f87e10ce33c0edd3
Norman <normanndot@gmail.com>
2020-10-10 11:54:11 +0530

    Initial Commit

---
+++ SwiftUICLEAN/SwiftUICLEAN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICLEAN
+//
+//  Created by Norman, ThankaVijay on 10/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICLEAN/SwiftUICLEAN/SwiftUICLEANApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICLEANApp.swift
+//  SwiftUICLEAN
+//
+//  Created by Norman, ThankaVijay on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICLEANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
